31b3556a1bf368d942e30ea450cb121d8e53c187
ahnsam207 <2ok25@daum.net>
2026-04-15 09:30:08 +0000

    f201

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 pages = [
     st.Page("pages/0_home.py", title="소개하기", icon="👩‍🏫"),
     st.Page("pages/1_data.py", title="활용한 데이터", icon="📂"),
