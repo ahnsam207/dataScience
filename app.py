bb9d4f44431dc76e05ef6abb69b596c165239d8b
ahnsam207 <2ok25@daum.net>
2026-04-15 07:54:13 +0000

    f201

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
 pages = [
-    st.Page("pages/0_home.py", title="시작하기", icon="👩‍🏫"),
-    st.Page("pages/1_data.py", title="데이터 수집", icon="📊"),
-    st.Page("pages/2_linear.py", title="다중선형 회귀분석", icon="📈"),
-    st.Page("pages/3_logistic.py", title="로지스틱 회귀분석", icon="📉"),
+    st.Page("pages/0_home.py", title="소개하기", icon="👩‍🏫"),
+    st.Page("pages/1_data.py", title="활용한 데이터", icon="📂"),
+    st.Page("pages/2_subject.py", title="연구 주제", icon="🔍"),
+    st.Page("pages/3_linear.py", title="다중 선형 회귀분석", icon="📈"),
+    st.Page("pages/4_logistic.py", title="로지스틱 회귀분석", icon="📉"),
 ]
 pg = st.navigation(pages)
 
