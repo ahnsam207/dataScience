e9f2119a85b195e75704a73c078b6ba2ccdcb1a5
ahnsam207 <2ok25@daum.net>
2026-04-21 11:32:39 +0900

    Update 3_linear.py

--- pages/3_linear.py
+++ pages/3_linear.py
@@ -355,9 +355,9 @@ elif st.session_state.linear_page == "탐색적 데이터 분석":
         '</div>',
         unsafe_allow_html=True
     )
-    st.markdown('<div style="margin-bottom:20px;"></div>', unsafe_allow_html=True)
+
     st.markdown(
-        '<div style="display:flex; align-items:center; gap:2px; margin-bottom:16px;">'
+        '<div style="display:flex; align-items:center; gap:12px; margin-bottom:16px;">'
         '<span style="font-size:18px;">🔎</span>'
         '<span style="font-size:20px; font-weight:800; color:#1565c0;">이상치 재확인</span>'
         '</div>',
